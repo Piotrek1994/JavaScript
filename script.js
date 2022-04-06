2f05c89ac6e48e7eecc86ee900f7dbfe5f826a63
PiotrPrus <pprus94@gmail.com>
2022-04-06 21:24:09 +0200

    Task 5

--- script.js
+++ script.js
@@ -319,3 +319,20 @@ for (let i = 1; i <= number; i++) {
 }
 
 
+for (let i = 1; i <= number; i++) {
+    const liItem = document.createElement('li')
+    liItem.textContent = i
+    ulList.append(liItem)
+}
+for (let i = 1; i <= number; i++) {
+    const liItem = document.createElement('li')
+    liItem.textContent = i
+    ulList.append(liItem)
+}
+for (let i = 1; i <= number; i++) {
+    const liItem = document.createElement('li')
+    liItem.textContent = i
+    ulList.append(liItem)
+}
+
+
