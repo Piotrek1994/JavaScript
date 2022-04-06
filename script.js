6e5ad5b4daad892eff940ce121cb3f4c459a26c9
PiotrPrus <pprus94@gmail.com>
2022-04-06 20:04:43 +0200

    Querry Selector

--- script.js
+++ script.js
@@ -275,3 +275,16 @@
 //string[0].toUpperCase() + string.substring(1)
 
 
+{/* <h1>pobierz mnie do zmiennej 'heading'</h1>
+<p>pobierz mnie do elementu tablico-podobnego razem z innymi paragrafami!</p>
+<p>pobierz mnie do elementu tablico-podobnego razem z innymi paragrafami!</p>
+<p>pobierz mnie do elementu tablico-podobnego razem z innymi paragrafami!</p>
+<div class="test">
+    <p>pobierz DIV-a z klasą test do jakiejś zmiennej</p>
+    <p id="test">następnie pobierz ten paragraf, ale nie przeszukuj całego `document`, przeszukaj tylko DIV-a z klasą test</p>
+</div> */}
+
+const heading = document.querySelector('h1')
+const paragraf = document.querySelectorAll('p')
+const div = document.querySelector('.test')
+const pDiv = document.querySelector('.test p')
