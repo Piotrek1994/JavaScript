25f2d7dd70a4b3b9e3e3bc77f0deec30a583634d
Piorek1994 <pprus94@gmail.com>
2022-04-11 20:00:22 +0200

    task

--- script.js
+++ script.js
@@ -533,3 +533,11 @@
 // 	.then(data => img.setAttribute('src', data.message))
 // 	.catch(err => console.log(err))
 // })
+
+
+// btn.addEventListener('click',  () => {
+// 	fetch(URL)
+// 	.then(res => res.json())
+// 	.then(data => img.setAttribute('src', data.message))
+// 	.catch(err => console.log(err))
+// })
